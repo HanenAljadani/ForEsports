bf3c9e5041edefecc437f2fd81f274cd2ec5ddd8
HanenAljadani <haneenabdullah66@hotmail.com>
2023-09-16 17:07:54 +0300

    m

--- app/Components/navbar.jsx
+++ app/Components/navbar.jsx
@@ -11,13 +11,13 @@ function Navbar() {
    
       <nav class="flex justify-between bg-gray-900 text-white w-screen">
         <div class="px-5 xl:px-12 py-6 flex w-full items-center">
-          <a class="text-3xl font-bold font-heading" href="/">
+          <a class="text-3xl font-bold font-heading" href="#">
             Esport<span className="text-purple-500">s</span> Blog
           </a>
           <ul class="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
             <li><a class="hover:text-gray-200" href="/home">Home</a></li>
-            <li><a class="hover:text-gray-200" href="/">Blog</a></li>
-            <li><a class="hover:text-gray-200" href="/">Contact Us</a></li>
+            <li><a class="hover:text-gray-200" href="#">Blog</a></li>
+            <li><a class="hover:text-gray-200" href="#">Contact Us</a></li>
      
             <li><Link class="hover:text-gray-200" href="/Auth/login">Login</Link></li>
             <li><Link class="hover:text-gray-200" href="/Auth/signup">Signup</Link></li>
