101d637b54830617aad419c75480502f780df901
HanenAljadani <haneenabdullah66@hotmail.com>
2024-05-01 14:21:53 +0300

    Edit: Footer

--- app/Components/footer.jsx
+++ app/Components/footer.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 export default function footer() {
   return (
-    <div className=''>
+    <div className='bg-gray-900 h-[100px] 	'>
      <div>
-      <h3 className='font-light text-l '>Where the dreamer are</h3>
+      <h3 className='text-stone-200 font-bold text-l pt-[30px] flex justify-center items-center drop-shadow-lg'>Where the dreamers are</h3>
+      <h3><a className='hover:underline hover:decoration-sky-300 text-xs text-stone-400 pt-4 flex justify-center items-center drop-shadow-lg cursor-pointer' href='#'>Contact</a></h3>
      </div>
 
     </div>
