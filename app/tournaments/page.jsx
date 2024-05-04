0134a08e0f5359d3cb643352adfc5061477a7e3f
HanenAljadani <haneenabdullah66@hotmail.com>
2024-05-04 16:47:48 +0300

    Fix: deployment error replace ' to &apos;

--- app/tournaments/page.jsx
+++ app/tournaments/page.jsx
@@ -12,9 +12,9 @@ function tournaments(){
       
       <div class="relative">
         <img class="w-full rounded-xl" src="https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="Colors" />
-        <p class="absolute top-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-br-lg rounded-tl-lg">Tournament's Game</p>
+        <p class="absolute top-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-br-lg rounded-tl-lg">Tournament&apos;s Game</p>
       </div>
-      <h1 class="mt-4 text-gray-800 text-2xl font-bold cursor-pointer">Tournament's Name</h1>
+      <h1 class="mt-4 text-gray-800 text-2xl font-bold cursor-pointer">Tournament&apos;s Name</h1>
       <div class="my-4">
         <div class="flex space-x-1 items-center">
           <span>
@@ -22,7 +22,7 @@ function tournaments(){
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
           </span>
-          <p>Tournament's Time</p>
+          <p>Tournament&apos;s Time</p>
         </div>
         <div class="flex space-x-1 items-center">
           <span>
@@ -30,7 +30,7 @@ function tournaments(){
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
             </svg>
           </span>
-          <p>Tournament's Prize</p>
+          <p>Tournament&apos;s Prize</p>
         </div>
         <div class="flex space-x-1 items-center">
           <span>
@@ -38,7 +38,7 @@ function tournaments(){
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
             </svg>
           </span>
-          <p>Tournament's Location</p>
+          <p>Tournament&apos;s Location</p>
         </div>
         <button class="mt-4 text-xl w-full text-white hover:bg-indigo-500 bg-indigo-600 py-2 rounded-xl shadow-lg">Enroll</button>
       </div>
